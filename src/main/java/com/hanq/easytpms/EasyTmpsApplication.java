6a89bb079fbf43a8878bc4fede6e56785ff47041
Juyoung Chung <84014277+dilatata@users.noreply.github.com>
2022-06-09 17:37:49 +0900

    name change

--- src/main/java/com/hanq/easytpms/EasyTmpsApplication.java
+++ src/main/java/com/hanq/easytpms/EasyTmpsApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class EasyTmpsApplication {
+public class EasyTPMSApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(EasyTmpsApplication.class, args);
